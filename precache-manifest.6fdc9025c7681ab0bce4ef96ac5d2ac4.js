96a922bcdae2c28033649c376f5375a02188c664
AGUIRI <rayyane.aguiri@etud.univ-jfc.fr>
2020-03-28 01:04:59 -0400

    Updates

---
+++ precache-manifest.6fdc9025c7681ab0bce4ef96ac5d2ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733a5b5aae1f6948da36cc8788813943",
+    "url": "/React-ToDoList-Rayyane-AGUIRI/index.html"
+  },
+  {
+    "revision": "98e44f6257834c822808",
+    "url": "/React-ToDoList-Rayyane-AGUIRI/static/css/main.714afd39.chunk.css"
+  },
+  {
+    "revision": "0ac260c2a0447a8d1547",
+    "url": "/React-ToDoList-Rayyane-AGUIRI/static/js/2.c77b0b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDoList-Rayyane-AGUIRI/static/js/2.c77b0b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e44f6257834c822808",
+    "url": "/React-ToDoList-Rayyane-AGUIRI/static/js/main.6c5b8e27.chunk.js"
+  },
+  {
+    "revision": "6a3d07fce7642c7a2e7b",
+    "url": "/React-ToDoList-Rayyane-AGUIRI/static/js/runtime-main.111e2ab9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-ToDoList-Rayyane-AGUIRI/static/media/logo.5d5d9eef.svg"
+  }
+]);
